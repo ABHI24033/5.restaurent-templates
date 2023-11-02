e58e547d3dce5b59a91dd0fd8b4b880c2c16b20d
Abhishek Kumar <abhi24033c@gmail.com>
2023-11-02 22:06:54 +0530

    first commit

---
+++ javascript/nav.js
@@ -0,0 +1,10 @@
+const bars=document.getElementById("bars");
+const nav=document.querySelector(".nav");
+
+bars.addEventListener("click",()=>{
+    bars.classList.toggle("fa-xmark")
+    // bars.classList.toggle("xmark")
+    // console.log("Hello world")
+    nav.classList.toggle("toggle")
+    // nav.s
+})
